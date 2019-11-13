5728f46f5a206d5cd8952478023eb3437f8458d5
Hannes Kirsman <hannes.kirsman@wunder.io>
2019-11-13 23:13:18 +0200

    Add some skeleton examples (potential structure).

---
+++ exercises/00_simple_demo.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @file
+ * Simple introductory file for initial demo.
+ *
+ * @tood: Add code with 1 issue?
+ */
---
+++ exercises/03_etc.php
@@ -0,0 +1,5 @@
+One logical way to separate the exercises would be to separate by tasks.
+
+Then again having separate topic for things like cognitive complexity would be
+nice.
+
